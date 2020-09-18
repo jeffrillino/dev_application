ae9fdb67a64f64734455921eeaab778f6d150aa9
jeffrillino <jeffry.amachree@gmail.com>
2020-09-18 18:11:32 +0100

    Title Changed
    Changed Title and port

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const path = require("path");
  * App Variables
  */
 const app = express();
-const port = process.env.PORT || "8000";
+const port = process.env.PORT || "9000";
 
 /**
  *  App Configuration
